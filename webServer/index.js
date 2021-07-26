f74a06fb2305d5aa1e90e06a879513374b501a88
Jose Medellin <kabo.0699@gmail.com>
2021-07-26 15:59:26 -0500

    finished webserver

---
+++ webServer/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+const port = process.env.PORT;
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+// static content
+app.use(express.static('public/home'));
+
+app.get('/generic', (req, res) => {
+    res.render('elements', {
+        name: 'JoKaMexD',
+        title: 'Node course with handlebars'
+    });
+});
+
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        name: 'JoKaMexD',
+        title: 'Node course with handlebars'
+    });
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        name: 'JoKaMexD',
+        title: 'Node course with handlebars'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html');
+})
+
+app.listen(() => {
+    console.log('lISTENING APP IN ', port);
+});
